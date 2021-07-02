5bef58d2a743b2f7828d481d4c769028c94d3216
rhodeon <ruonabraks@gmail.com>
2021-07-03 00:58:16 +0100

    Implemented API request and response handling.

---
+++ lib/network/weather_api_client.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:weatherly/models/current_weather_model.dart';
+
+class OpenWeatherMapApiClient {
+  final String baseUrl = "api.openweathermap.org";
+  final String currentWeatherEndpoint = "data/2.5/weather/";
+  final String apiKey = "011fcf65427e360e15f1a22132399386";
+
+  Map<String, String> setQueryParameters(
+    // Query parameters for current weather
+    String city,
+    String countryCode,
+  ) =>
+      {
+        "q": "$city, $countryCode",
+        "appid": "$apiKey",
+      };
+
+  Future<CurrentWeatherModel> getCurrentWeather({
+    // Requests for the current weather
+    required String city,
+    required String countryCode,
+  }) async {
+    final response = await Client().get(
+      Uri.https(
+        baseUrl,
+        currentWeatherEndpoint,
+        setQueryParameters(city, countryCode),
+      ),
+    );
+    return CurrentWeatherModel.fromJson(json.decode(response.body));
+  }
+}
